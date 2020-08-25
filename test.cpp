743ad295c050a441fefc6e77b3ea14884dfcf086
egorsmth <mattspring0@gmail.com>
2020-08-25 20:52:42 +0300

    init

--- test.cpp
+++ test.cpp
@@ -15,56 +15,104 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 // The MAIN function, from here we start the application and run the game loop
-int main()
+
+void DrawShit() {
+    glBegin(GL_TRIANGLES);
+        glColor3f( 0, 1, 1 );
+        glVertex3f(-1, 1, 0.5);   // closer
+        glVertex3f(-1, 0.5, 0.5);
+        glVertex3f(-0.9, 1, 0.5);
+
+        glColor3f( 1, 1, 1 );
+        glVertex3f(-1, 1, 0.6);   // futher
+        glVertex3f(-0.5, 1, 0.6);
+        glVertex3f(-1, 0.9, 0.6);
+
+
+        glColor3f( 1, 0.5, 1 );
+        glVertex3f(-0.7, 1, 0.3);
+        glVertex3f(-1, 0.7, 0.7);
+        glVertex3f(-0.9, 0.6, 0.7);
+
+    glEnd();
+}
+
+void DrawStar() {
+    glBegin(GL_TRIANGLE_FAN);
+        glColor3f( 0, 1, 1 );
+        glVertex2f(0, 0);
+
+        glColor3f( 1, 1, 1 );
+        glVertex2f(0, 0.5);
+
+        glColor3f( 0, 1, 1 );
+        glVertex2f(-0.1, 0.1);
+
+        glColor3f( 1, 1, 1 );
+        glVertex2f(-0.5, 0.1);
+
+        glColor3f( 0, 1, 1 );
+        glVertex2f(-0.15, -0.05);
+
+        glColor3f( 1, 1, 1 );
+        glVertex2f(-0.3, -0.5);
+        
+        glColor3f( 0, 1, 1 );
+        glVertex2f(0, -0.15);
+
+        glColor3f( 1, 1, 1 );
+        glVertex2f(0.3, -0.5);
+
+        glColor3f( 0, 1, 1 );
+        glVertex2f(0.15, -0.05);
+
+        glColor3f( 1, 1, 1 );
+        glVertex2f(0.5, 0.1);
+
+        glColor3f( 0, 1, 1 );
+        glVertex2f(0.1, 0.1);
+
+        glColor3f( 1, 1, 1 );
+        glVertex2f(0, 0.5);
+    glEnd();
+}
+
+int main(int argc, char** argv)
 {
-    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-    // Init GLFW
-    glfwInit();
-    // Set all the required options for GLFW
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window;
 
-    // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);    
-    if (window == nullptr)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
+	glfwInit();
+	window = glfwCreateWindow(640, 480, argv[0], NULL, NULL);
+	glfwMakeContextCurrent(window);
 
-    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
-    glewExperimental = GL_TRUE;
-    // Initialize GLEW to setup the OpenGL Function pointers
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }    
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable( GL_DEPTH_TEST );
 
-    // Define the viewport dimensions
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);  
-    glViewport(0, 0, width, height);
-
-    // Game loop
     while (!glfwWindowShouldClose(window))
     {
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glColor3f(1, 1, 1);
+
+        glLineWidth( 1 ); // Lines are n pixels wide
+        glEnable(GL_LINE_SMOOTH);
+        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST); // Antialias lines
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glShadeModel( GL_FLAT ); // or GL_SMOOTH
+
+        DrawStar();
+
+        DrawShit();
+
+        glFlush();
+        glfwSetTime(0);
+            // Swap the screen buffers
+        glfwSwapBuffers(window);
         // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
         glfwPollEvents();
-
-        // Render
-        // Clear the colorbuffer
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swap the screen buffers
-        glfwSwapBuffers(window);
     }
 
     // Terminate GLFW, clearing any resources allocated by GLFW.
